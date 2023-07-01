29c19a66b3a547f8028b178a61394d6d10071c40
Oleg Plugaru <oleg.plugaru.md@gmail.com>
2023-07-01 12:16:53 +0300

    finished string and characters

--- StringsAndCharacters.playground/Contents.swift
+++ StringsAndCharacters.playground/Contents.swift
@@ -76,7 +76,7 @@ print(catString)
 
 let string1 = "hello"
 let string2 = " there"
-var welcome = string1 + string2
+//var welcome = string1 + string2
 // welcome now equals "hello there"
 
 var instruction = "look over"
@@ -132,3 +132,150 @@ let enclosedEAcute: Character = "\u{E9}\u{20DD}"
 // enclosedEacute is é⃝
 let regianalIndicatorForUS: Character = "\u{1F1FA}\u{1F1F8}"
 // regionalIndicatorForUS is 🇺🇸
+
+let unusualMenagerie = "Koala 🐨, Snail 🐌, Penguin 🐧, Dromedary 🐪"
+print("unusualMenagerie has \(unusualMenagerie.count) characters")
+// Prints "unusualMenagerie has 40 characters"
+
+var word = "cafe"
+print("the number of characters in \(word) is \(word.count)")
+// Prints "the number of characters in cafe is 4"
+
+word += "\u{301}" // COMBINING ACUTE ACCENT, U+0301
+
+print("the number of characters in \(word) is \(word.count)")
+// Prints "the number of characters in café is 4"
+
+//let greeting = "Guten Tag!"
+//greeting[greeting.startIndex]
+// G
+//greeting[greeting.index(before: greeting.endIndex)]
+// !
+//greeting[greeting.index(after: greeting.startIndex)]
+// u
+//let index = greeting.index(greeting.startIndex, offsetBy: 7)
+//greeting[index]
+// a
+
+//greeting[greeting.endIndex] // Error
+//greeting.index(after: greeting.endIndex) // Error
+
+for index in greeting.indices {
+    print("\(greeting[index]) ", terminator: "")
+}
+// Prints "G u t e n  T a g !"
+
+var welcome = "hello"
+welcome.insert("!", at: welcome.endIndex)
+// welcome now equals "hello!"
+
+welcome.insert(contentsOf: " there", at: welcome.index(before: welcome.endIndex))
+// welcome now equals "hello there!"
+
+
+welcome.remove(at: welcome.index(before: welcome.endIndex))
+// welcome now equals "hello there"
+
+let range = welcome.index(welcome.endIndex, offsetBy: -6)..<welcome.endIndex
+welcome.removeSubrange(range)
+// welcome now equals "hello"
+
+let greeting = "Hello, world!"
+let index = greeting.firstIndex(of: ",") ?? greeting.endIndex
+let beginning = greeting[..<index]
+// beginning is "Hello"
+
+// Convert the result to a String for long-term storage.
+let newString = String(beginning)
+
+let quotation = "We're a lot alike, you and I."
+let sameQuotation = "We're a lot alike, you and I."
+if quotation == sameQuotation {
+    print("These two strings are considered equal")
+}
+// Prints "These two string are considered equal"
+
+
+// "Voulez-vous un café?" using LATIN SMALL LETTER E WITH ACUTE
+let eAcuteQuestion = "Voulez-vous un caf\u{E9}?"
+
+// "Voulez-vous un café?" using LATIN SMALL LETTER E and COMBINING ACUTE ACCENT
+let combinedEAcuteQuestion = "Voulez-vous un caf\u{65}\u{301}?"
+
+if eAcuteQuestion == combinedEAcuteQuestion {
+    print("These two strings are considered equal")
+}
+// Prints "These two strings are considered equal"
+
+let latinCapitalLetterA: Character = "\u{41}"
+
+let cyrillicCapitalLetterA: Character = "\u{0410}"
+
+if latinCapitalLetterA != cyrillicCapitalLetterA {
+    print("These two characters aren't equivalent.")
+}
+// Prints "These two characters aren't equivalent."
+
+let romeoAndJuliet = [
+"Act 1 Scene 1: Verona, A public place",
+"Act 1 Scene 2: Capulet's mansion",
+"Act 1 Scene 3: A room in Capulet's mansion",
+"Act 1 Scene 4: A street outside Capulet's mansion",
+"Act 1 Scene 5: The Great Hall in Capulet's mansion",
+"Act 2 Scene 1: Outside Capulet's mansion",
+"Act 2 Scene 2: Capulet's orchard",
+"Act 2 Scene 3: Outside Friar Lawrence's cell",
+"Act 2 Scene 4: A street in Verona",
+"Act 2 Scene 5: Capulet's mansion",
+"Act 2 Scene 6: Friar Lawrence's cell"
+]
+
+var act1SceneCount = 0
+for scene in romeoAndJuliet {
+    if scene.hasPrefix("Act 1 ") {
+        act1SceneCount += 1
+    }
+}
+print("There are \(act1SceneCount) scenes in Act 1")
+// Prints "There are 5 scenes in Act 1"
+
+var mansionCount = 0
+var cellCount = 0
+for scene in romeoAndJuliet {
+    if scene.hasSuffix("Capulet's mansion") {
+        mansionCount += 1
+    } else if scene.hasSuffix("Friar Lawrence's cell") {
+        cellCount += 1
+    }
+}
+print("\(mansionCount) mansion scenes; \(cellCount) cell scenes")
+// Prints "6 mansion scenes; 2 cell scenes"
+
+let dogString = "Dog!!🐶"
+
+for codeUnit in dogString.utf8 {
+    print("\(codeUnit)", terminator: "")
+}
+print("")
+// Prints "68 111 103 226 128 188 240 159 144 182 "
+
+for codeUnit in dogString.utf16 {
+    print("\(codeUnit) ", terminator: "")
+}
+print("")
+// Prints "68 111 103 8252 55357 56374 "
+
+for scalar in dogString.unicodeScalars {
+    print("\(scalar.value) ", terminator: "")
+}
+print("")
+// Prints "68 111 103 8252 12805 "
+
+for scalar in dogString.unicodeScalars {
+    print("\(scalar) ")
+}
+// D
+// o
+// g
+// !!
+// 🐶
